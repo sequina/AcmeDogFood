8fcb043982736e2d995941766599b6415d757fc1
Sequina Caro <sccaro89@gmail.com>
2015-09-21 21:07:01 -0500

    css/bootstrap styling

---
+++ javascripts/main.js
@@ -0,0 +1,4 @@
+define(["jquery", "bootstrap", "hbs", "populate-songs", "getUnique", "firebase", "getTemplates"],
+function($, bootstrap, handlebars, populateSongs, getUnique, firebase, templates) {
+
+});
