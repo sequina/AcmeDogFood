43ca4184a3f5b69bdc20ee304e601f6de09b1073
Chris <cl.anderson8@gmail.com>
2015-09-23 17:36:04 -0500

    alternate index, working templates

--- javascripts/getTemplates.js
+++ javascripts/getTemplates.js
@@ -1,15 +1,17 @@
-define(["hbs", "hbs!../templates/doogfood", "hbs!../templates/catfood"],
-function(handlebars, dogTmpl, catTmpl) {
+    define(["hbs",
+                "hbs!../templates/dogfood",
+                "hbs!../templates/catfood"],
+    function(handlebars, dogTmpl, catTmpl) {
 
-  return {
-    dogTmpl: function(fbObject) {
-      // console.log("songTmpl is here!");
-      return dogTmpl(fbObject);
-    },
-    catTmpl: function(fbObject) {
-      // console.log("artistTmpl is here!");
-      return catTmpl(fbObject);
-    }
-  };
+      return {
+        dogTmpl: function(fbObject) {
+          // console.log("dogTmpl is here!");
+          return dogTmpl(fbObject);
+        },
+        catTmpl: function(fbObject) {
+          // console.log("catTmpl is here!");
+          return catTmpl(fbObject);
+        }
+      };
 
-});
+    });
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,27 @@
-define(["jquery", "bootstrap", "hbs", "dogfood"],
-function($, bootstrap, handlebars, dogfoodTmpl) {
-return {
-  $.ajax({url: "./dogfood.json"}).done(
-    function(){
-      $(".output").html(dogfoodTmpl(./dogfood.json))
-    })
-}
+define(["jquery", "bootstrap", "hbs", "firebase", "getTemplates"],
+function($, bootstrap, handlebars, firebase, templates) {
+
+    var dogfood = [];
+    var catfood = [];
+
+    var myFirebaseRef = new Firebase("https://acmedogfood.firebaseio.com/");
+
+    // console.log(myFirebaseRef);
+
+    myFirebaseRef.child("dog_brands").on("value", function(snapshot) {
+        dogfood = snapshot.val();
+        });
+
+    myFirebaseRef.child("cat_brands").on("value", function(snapshot) {
+        catfood = snapshot.val();
+    });
+
+    $("#dog").click(function() {
+        $(".output").html(templates.dogTmpl({dog_brands:dogfood}));
+    });
+
+    $("#cat").click(function() {
+        $(".output").html(templates.catTmpl({cat_brands:catfood}));
+    });
 
 });
